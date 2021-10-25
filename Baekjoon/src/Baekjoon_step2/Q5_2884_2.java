bbdc26468014e753514acc46c2fd257310c5bb7f
Jun <92867951+Kosmouser94@users.noreply.github.com>
2021-10-25 13:05:34 +0900

    Update Q5_2884_2.java

--- Baekjoon/src/Baekjoon_step2/Q5_2884_2.java
+++ Baekjoon/src/Baekjoon_step2/Q5_2884_2.java
@@ -6,20 +6,32 @@ package Baekjoon_step2;
 
 import java.util.*;
 
-public class Q5_2885 {
+public class Q5_2884_2 {
+
 	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int H = sc.nextInt();
 		int M = sc.nextInt();
 		int alarm = -45;
-		int Time = (H*60) + M + alarm;
-	sc.close();
-			
-			
-		if (Time<0) {
-			Time += 1440;
-		} 
+		int Time = (H * 60) + M + alarm;
+		sc.close();
 
-		System.out.println(Time/60 + " " + Time%60);
+		if (Time < 0) { // 시간이 음수일 때
+			if (Time < -1440) { // 시간이 음수면서 24시간을 초과할 때
+				Time = (Time % -1440) + 1440;
+			} else {
+				Time += 1440;
+			}
+		}
+
+		if (Time > 1440) { // 시간이 24시간을 초과할 때
+			Time = (Time % 1440);
+		}
+
+		if (Time / 60 == 24) { // 시침이 24일 때
+			Time %= 1440;
+		}
+
+		System.out.println(Time / 60 + " " + Time % 60);
 	}
 }
